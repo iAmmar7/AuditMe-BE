6a33e741e6b72633fa8e658af7c62852e177634b
iammar7 <maansaridot@gmail.com>
2020-12-02 02:15:45 +0500

    report chart issue resolving

--- routes/user.js
+++ routes/user.js
@@ -477,7 +477,7 @@ router.get('/report-chart', async (req, res) => {
   } catch (error) {
     return res.status(400).json({
       success: false,
-      message: 'Unknown error',
+      message: error,
     });
   }
 });
