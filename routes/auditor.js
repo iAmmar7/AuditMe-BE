1e11f6e08d4435ddad38f1858ac4eb3c7e45dad8
iammar7 <maansaridot@gmail.com>
2020-12-08 01:45:11 +0500

    initiative report edit api

--- routes/auditor.js
+++ routes/auditor.js
@@ -339,4 +339,95 @@ router.post('/initiative', async (req, res) => {
   });
 });
 
+// @route   POST /api/auditor/update-initiative/:id
+// @desc    Update initiative report
+// @access  Private
+router.post('/update-initiative/:id', async (req, res) => {
+  const formData = formidable({
+    uploadDir: './public/initiatives',
+    keepExtensions: true,
+    multiples: true,
+  });
+
+  if (!req.params.id)
+    return res.status(400).json({ success: false, message: 'Initiative report id is required' });
+
+  const report = await Initiatives.findOne({ _id: req.params.id });
+
+  if (!report) return res.status(400).json({ success: false, message: 'Unable to update report' });
+
+  if (report.user.toString() !== req.user.id.toString())
+    return res
+      .status(400)
+      .json({ success: false, message: 'You are not authorized to update this initiative' });
+
+  formData.parse(req, async (error, fields, files) => {
+    const { evidencesBefore, evidencesAfter } = files;
+    try {
+      if (error) throw 'Unable to upload image!';
+
+      let arrayOfEvidencesBeforeFiles = [],
+        arrayOfEvidencesAfterFiles = [];
+
+      if (evidencesBefore) {
+        Object.keys(evidencesBefore).forEach((value) => {
+          if (evidencesBefore[value] && evidencesBefore[value].path) {
+            const path = evidencesBefore[value].path.split('public')[1];
+            arrayOfEvidencesBeforeFiles.push(path);
+          }
+          if (value === 'path') {
+            const path = evidencesBefore[value].split('public')[1];
+            arrayOfEvidencesBeforeFiles.filter((item) => {
+              if (item !== path) arrayOfEvidencesBeforeFiles.push(path);
+            });
+          }
+        });
+      }
+
+      if (evidencesAfter) {
+        Object.keys(evidencesAfter).forEach((value) => {
+          if (evidencesAfter[value] && evidencesAfter[value].path) {
+            const path = evidencesAfter[value].path.split('public')[1];
+            arrayOfEvidencesAfterFiles.push(path);
+          }
+          if (value === 'path') {
+            const path = evidencesAfter[value].split('public')[1];
+            arrayOfEvidencesAfterFiles.filter((item) => {
+              if (item !== path) arrayOfEvidencesAfterFiles.push(path);
+            });
+          }
+        });
+      }
+
+      console.log(arrayOfEvidencesBeforeFiles);
+      console.log(arrayOfEvidencesAfterFiles);
+
+      let updatedEvidencesBefore = [...report.evidencesBefore, ...arrayOfEvidencesBeforeFiles];
+      let updatedEvidencesAfter = [...report.evidencesAfter, ...arrayOfEvidencesAfterFiles];
+
+      // Update db
+      const updateReport = await Initiatives.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+          ...fields,
+          evidencesBefore: updatedEvidencesBefore,
+          evidencesAfter: updatedEvidencesAfter,
+        },
+        { new: true },
+      );
+
+      if (!updateReport) throw 'Unable to update the report';
+
+      return res.status(200).json({ success: true, report: updateReport });
+    } catch (error) {
+      if (evidencesBefore) fs.unlinkSync(evidencesBefore.path);
+      if (error && error.name === 'ValidationError') {
+        return res.status(400).json({ success: false, message: 'Input fields validation error' });
+      }
+
+      return res.status(400).json({ success: false, message: error });
+    }
+  });
+});
+
 module.exports = router;
